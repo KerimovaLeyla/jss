0b3149f68d355682e96cdeb4bd3a8e61d95d8606
KerimovaLeyla <sd79pqh0s@gmail.com>
2023-10-20 09:18:17 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+let students = [
+
+    { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+    { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+    { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+    { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+    { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+    { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+    { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+    { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+    { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 }
+
+]
+/* TASK1
+
+let a=20;
+        let b=10;
+        if (a>b)
+         { 
+            console.log("a b-den boyukdur");
+        }
+         else if(a === b)
+          { 
+            console.log("a b-ye beraberdir");
+        }
+          else {
+            console.log("a b-den kicikdir");
+        }
+
+    TASK2
+
+let evenindex=[];
+for (let i = 0; i< students.length; i++) 
+{
+    if (students[i].id%2===0) {
+evenindex.push(students[i]);
+}}
+console.log(evenindex);
+
+TASK3
+
+let oddindex=[];
+for (let i = 0; i< students.length; i++) 
+{
+    if (students[i].id%2===1) 
+    {
+oddindex.push(students[i]);
+}}
+console.log(oddindex);
+
+TASK4
+ 
+let telebe=[];
+for (let i = 0; i< students.length; i++) 
+{
+    if (students[i].age>20) {
+telebe.push(students[i]);
+}}
+console.log(telebe);
+
+ 
+TASK5
+
+let twocondition=[];
+for (let i = 0; i< students.length; i++) 
+{
+    if (students[i].id%2===0 && students[i].grade>90) 
+   
+ {twocondition.push(students[i]);}
+} 
+ 
+console.log(twocondition);
+
+
+
+TASK6
+
+let or=[];
+for (let i = 0; i< students.length; i++) 
+{if (students[i].grade==100 || students[i].grade==95) {
+or.push(students[i]);
+}} 
+ 
+console.log(or);
+*/
